4d2f55aed675b20e931dff37ea8a07155504982e
seuna <seuna@LAPTOP-3O34I0JT>
2020-04-11 21:42:14 +0100

    add more step

--- CSS/src/test/java/com/project/Steps/Step.java
+++ CSS/src/test/java/com/project/Steps/Step.java
@@ -1,20 +1,26 @@
 package com.project.Steps;
 
 import com.project.Helper.BaseClass;
+import com.project.Pages.HomePage;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class Step extends BaseClass 
 {
+	HomePage homepage = new HomePage();
+	
+	
 	@Given("^I navigate to web$")
 	public void i_navigate_to_web() throws Throwable {
+		launchUrl("https://app.clearscore.com/login");
 	    
 	}
 
 	@Then("^i input email$")
 	public void i_input_email() throws Throwable {
-	    
+		
+	    homepage.andinputemail();
 	}
 
 
